5c65de4bc927fca774c55d01260cb609f03a8ede
sarajj13781 <sarafarzaneh1378@gmail.com>
2021-08-10 19:34:32 +0430

    added jwt-auth package

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
         $this->call(PatientsSeeder::class);
+        $this->call(UserSeeder::class);
     }
 }
